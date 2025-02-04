ef44ff733c62d2c1b5c30d39f7c171ab813f393c
pravinsankar3 <pravinchamp98@gmail.com>
2025-02-04 16:28:07 +0530

    updating test1

--- src/main/java/org/example/javaPrograms/test1.java
+++ src/main/java/org/example/javaPrograms/test1.java
@@ -2,15 +2,17 @@ package org.example.javaPrograms;
 
 public class test1 {
 
-    public static void revNum(int n){
-        int rev = 0;
-        while (n!=0){
-
+    public static void revSentence(String s) {
+        String[] ns = s.split(" ");
+        String rev = "";
+        for (int i = ns.length - 1; i >= 0; i--) {
+            rev = rev + ns[i] + " ";
         }
+        System.out.println("Reversed sentence is: " + rev);
     }
 
-    public static void main(String[] args){
-        int num = 35789;
-        revNum(num);
+    public static void main(String[] args) {
+        String s = "I like the view";
+        revSentence(s);
     }
 }
