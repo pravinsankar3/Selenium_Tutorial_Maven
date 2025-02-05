e9ebaf4dabdff6048101285b276fc53f16231679
pravinsankar3 <pravinchamp98@gmail.com>
2025-02-05 12:42:47 +0530

    updating test1.java

--- src/main/java/org/example/javaPrograms/test1.java
+++ src/main/java/org/example/javaPrograms/test1.java
@@ -1,24 +1,17 @@
 package org.example.javaPrograms;
 
+import java.util.Arrays;
+
 public class test1 {
 
     public static void main(String[] args) {
         int[] a = {2, 4, 6, 3, 8};
-        secLargest(a);
+        sortArray(a);
     }
 
-    public static void secLargest(int[] a) {
-        int lar = Integer.MIN_VALUE;
-        int sec = Integer.MIN_VALUE;
+    public static void sortArray(int[] a){
+        Arrays.sort(a);
 
-        for (int b : a) {
-            if (b > lar) {
-                sec = lar;
-                lar = b;
-            } else if (b > sec) {
-                sec = b;
-            }
-        }
-        System.out.println("Second largest number in the array is: " + sec);
+        System.out.println("Sorted array: "+Arrays.toString(a));
     }
 }
